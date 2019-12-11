a0ac0ee37360268e33f6b18b3b16df009ee1048a
maming <maming@xmi01.com>
2019-12-11 17:27:55 +0800

    优化

--- src/main/java/com/datahub/zipkin/ZipkinApplication.java
+++ src/main/java/com/datahub/zipkin/ZipkinApplication.java
@@ -1,14 +1,12 @@
 package com.datahub.zipkin;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import zipkin2.server.internal.EnableZipkinServer;
 import zipkin2.storage.mysql.v1.MySQLStorage;
-import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableZipkinServer
@@ -19,8 +17,6 @@ public class ZipkinApplication {
         SpringApplication.run(ZipkinApplication.class, args);
     }
 
-    @ConfigurationProperties(prefix = "spring.datasource")
-    @Primary
     @Bean
     public MySQLStorage mySQLStorage(DataSource datasource) {
         return MySQLStorage.newBuilder().datasource(datasource).executor(Runnable::run).build();
